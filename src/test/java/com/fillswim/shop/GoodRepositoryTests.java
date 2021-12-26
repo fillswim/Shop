74273c1c0f148a26b5ed4bc8f93ab1a4e29814e1
Oleg Filatov <fillswim@gmail.com>
2021-12-26 15:51:13 +0300

    Add tests

---
+++ src/test/java/com/fillswim/shop/GoodRepositoryTests.java
@@ -0,0 +1,104 @@
+package com.fillswim.shop;
+
+import com.fillswim.shop.Controller.RESTController;
+import com.fillswim.shop.DAO.GoodRepository;
+import com.fillswim.shop.Entity.Good;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class GoodRepositoryTests {
+
+    @Autowired
+    private RESTController restController;
+    @Autowired
+    private GoodRepository repository;
+
+    @Test
+    public void contextLoads() {
+        assertThat(restController).isNotNull();
+    }
+
+    @Test
+    @Order(1)
+    public void testCreate() {
+
+        int id = 4;
+
+        Good good = new Good();
+        good.setTitle("Iphone");
+        good.setPrice(2000);
+        good.setProducerCountry("USA");
+
+        repository.save(good);
+
+        assertEquals("Iphone",
+                repository
+                        .findById(id)
+                        .orElseThrow(() -> new EntityNotFoundException(id + " not found"))
+                        .getTitle());
+    }
+
+    @Test
+    @Order(2)
+    public void testReadAll() {
+        List<Good> goods = repository.findAll();
+        assertThat(goods).size().isGreaterThan(0);
+    }
+
+    @Test
+    @Order(3)
+    public void testSimpleGood() {
+
+        int id = 1;
+
+        Good good = repository
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(id + " not found"));
+
+        assertEquals(299, good.getPrice());
+    }
+
+    @Test
+    @Order(4)
+    public void testUpdate() {
+
+        int id = 1;
+
+        Good good = repository
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(id + " not found"));
+
+        good.setPrice(500);
+        repository.save(good);
+
+        assertEquals(500,
+                repository
+                        .findById(id)
+                        .orElseThrow(() -> new EntityNotFoundException(id + " not found"))
+                        .getPrice());
+    }
+
+    @Test
+    @Order(5)
+    public void testDelete() {
+
+        int id = 4;
+
+        repository.deleteById(id);
+        assertThat(repository.existsById(id)).isFalse();
+    }
+
+
+}
---
+++ src/test/java/com/fillswim/shop/RestAPITests.java
@@ -0,0 +1,154 @@
+package com.fillswim.shop;
+
+import com.fillswim.shop.DAO.GoodRepository;
+import com.fillswim.shop.Entity.Good;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class RestAPITests {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Autowired
+    private GoodRepository repository;
+
+    private final String URL = "http://localhost:8080/api/goods";
+
+    @Test
+    @Order(1)
+    public void testGoodExistsInRepository() {
+
+        int id = 1;
+
+        // Load good from repository
+        Good goodFromRepository = repository.findById(id).orElse(null);
+
+        assertNotNull(goodFromRepository);
+    }
+
+    // Find good by id      GET     http://localhost:8080/api/goods{goodId}
+    @Test
+    @Order(2)
+    public void testGetGood() {
+        int id = 1;
+
+        // Load good with GET request
+        Good goodFromTemplate = restTemplate.getForObject(URL + "/" + id, Good.class);
+        System.out.println("goodFromTemplate: " + "\t" + goodFromTemplate);
+
+        // Load good from repository
+        Good goodFromRepository = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(id + " not found"));
+        System.out.println("goodFromRepository " + "\t" + goodFromRepository);
+
+        assertEquals(goodFromRepository, goodFromTemplate);
+    }
+
+    // Show all goods       GET     http://localhost:8080/api/goods
+    @Test
+    @Order(3)
+    public void testGetAllGoods() {
+
+        // Load list of goods with GET request
+        ResponseEntity<List<Good>> responseEntity =
+                restTemplate.exchange(URL, HttpMethod.GET, null,
+                        new ParameterizedTypeReference<>() {});
+
+        List<Good> goodsFromTemplate = responseEntity.getBody();
+        System.out.println(goodsFromTemplate);
+
+        // Load list of goods from repository
+        List<Good> goodsFromRepository = repository.findAll();
+        System.out.println(goodsFromRepository);
+
+        assertEquals(goodsFromRepository, goodsFromTemplate);
+    }
+
+    // Add good             POST    http://localhost:8080/api/goods
+    @Test
+    @Order(4)
+    public void testAddNewGood() {
+
+        int id = 4;
+        String title = "Nintendo switch";
+
+        // New good
+        Good good = new Good();
+        good.setTitle(title);
+        good.setPrice(399);
+        good.setProducerCountry("USA");
+
+        // Save new good
+        ResponseEntity<String> responseEntity =
+                restTemplate.postForEntity(URL, good, String.class);
+        System.out.println("New good was added to DB: " + responseEntity.getBody());
+
+        assertEquals(title,
+                repository
+                        .findById(id)
+                        .orElseThrow(() -> new EntityNotFoundException(id + " not found"))
+                        .getTitle());
+    }
+
+    // Update good          PUT     http://localhost:8080/api/goods
+    @Test
+    @Order(5)
+    public void testUpdateGood() {
+
+        int id = 3;
+        int newPrice = 1900;
+
+        // Load
+        Good good = restTemplate.getForObject(URL + "/" + id, Good.class);
+        if (good != null) {
+            good.setPrice(newPrice);
+
+            // Update
+            restTemplate.put(URL, good);
+        } else {
+            throw new EntityNotFoundException(id + " not found");
+        }
+
+        assertEquals(newPrice,
+                repository
+                        .findById(id)
+                        .orElseThrow(() -> new EntityNotFoundException(id + " not found"))
+                        .getPrice());
+
+    }
+
+    // Delete good          DELETE  http://localhost:8080/api/goods{goodId}
+    @Test
+    @Order(6)
+    public void testDeleteGood() {
+
+        int id = 3;
+
+        restTemplate.delete(URL + "/" + id);
+        System.out.println("Good with id = " + id + " was deleted");
+
+        assertNull(repository.findById(id).orElse(null));
+    }
+
+
+
+
+
+}
